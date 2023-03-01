40a259c7319951d130ebb93dc3aada5a4f2301cf
Killian-Susini <killian.susini@dauphine.eu>
2023-03-01 11:05:00 +0100

    starter code

---
+++ main.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+class GachaSlotMaker:
+    def __init__(self):
+        self.rates:dict[str,(int,float)] = dict()
+    
+    def set_rates(self, rates:dict[str,(int,float)]):
+        self.rates = rates
+        
+    def compute_stats(self):
+        pass
+    
+
+
+def main():
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
